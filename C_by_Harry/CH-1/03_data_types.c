62b1e16143db3d60f8fc086848c67daf90262ff4
Saiyam Gupta <saiyamg369@gmail.com>
2025-09-01 19:15:22 +0530

    sizeof fxn

--- C_by_Harry/CH-1/03_data_types.c
+++ C_by_Harry/CH-1/03_data_types.c
@@ -1,15 +1,16 @@
- #include <stdio.h>
+#include <stdio.h>
 
- int mian(){
-    //int a;
-    //a=1'
-    int a = 1; //Takes 4 bytes( 1byte = 8-bits)
+int main()
+{
+    // int a;
+    // a=1'
+    int a = 1; // Takes 4 bytes( 1byte = 8-bits)
 
-    //float b = 1.4;
+    // float b = 1.4;
     float b;
-    b = 1.4; //Takes 4 bytes
+    b = 1.4; // Takes 4 bytes
 
-    //char c ='a';
+    // char c ='a';
     char c;
-    c = 'a';//Always 1 byte
- }
+    c = 'a'; // Always 1 byte
+} // Note: use sizeof(<datatype>) fxn to get size of datatype
