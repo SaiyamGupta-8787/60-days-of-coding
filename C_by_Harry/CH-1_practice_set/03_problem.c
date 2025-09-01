950e2a80be5201a1a396e4bfe27af54897dcb1a6
Saiyam Gupta <saiyamg369@gmail.com>
2025-09-02 01:19:11 +0530

    chapter 2

---
+++ C_by_Harry/CH-1_practice_set/03_problem.c
@@ -0,0 +1,9 @@
+//Convert Celsius into Fahrenheit
+#include<stdio.h>
+
+int main(){
+    float c = 35.0,f;
+    f = ((9.0/5.0)*c) + 32;
+    printf("The value in fahrenheit is : %f",f);
+    return 0;
+}
