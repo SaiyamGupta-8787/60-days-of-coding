294be9e5d1aaff04738e3abf2fe8f5e382205235
Saiyam Gupta <saiyamg369@gmail.com>
2025-09-01 19:54:14 +0530

    theory end

---
+++ C_by_Harry/CH-1/05_comments.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    // This line is of no use (Single line comment)
+    /*
+    This is a 
+    multi-line
+    comment
+    */
+    printf("Hello World"); //Print Hello World
+    return 0; //Return 0 to the operating system
+}
---
+++ C_by_Harry/CH-1/07_escape_sequence.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    char ch = 'a';
+    ch = 'c';
+    ch = '\n'; //Represents a New Line
+    printf("Hello,\nhow are you\n?"); //Change Line
+    printf("Hello,\thow are you?\n"); //Give Space
+    printf("Hello,\"how are you?\" \n");
+    printf("Hello,\'how are you?\' \n"); //use 
+    return 0;
+}
