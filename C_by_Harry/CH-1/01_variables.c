30e5e9252959596a023f28ba707c4ec7992e1cdf
Saiyam Gupta <saiyamg369@gmail.com>
2025-09-01 19:02:16 +0530

    add datatypes

--- C_by_Harry/CH-1/01_variables.c
+++ C_by_Harry/CH-1/01_variables.c
@@ -2,7 +2,7 @@
 
 int main(){
     int a; //Variable declare
-    a = 6;//Variable Initialized
-    printf("%d",a);
+    a = 6; //Variable Initialized
+    printf("The output of program is : %d",a);
     return 0;
 }
---
+++ C_by_Harry/CH-1/02_variables_rules.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a;//Valid
+    int b;//Valid
+    int abc;//Valid
+    int harry_good;//Valid
+    //int 12;//Invalid
+    int saiyam$;//Invalid
+
+    return 0;
+}
